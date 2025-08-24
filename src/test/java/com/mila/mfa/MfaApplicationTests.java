3ce3f1c36e060e37c134a52614cc147f892dee47
mismetti <milastrokes@gmail.com>
2025-08-24 16:53:45 -0300

    commit inicial

---
+++ src/test/java/com/mila/mfa/MfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mila.mfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
